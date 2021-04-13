693089a681a49727ef62a467b6122a602c2693d1
the-panda-boo <Geo@1996>
2021-04-13 14:07:03 +0530

    Curcle Radius

--- Circle_area_from_radius.py
+++ Circle_area_from_radius.py
@@ -1,4 +1,4 @@
 from math import pi
-print("Program to calculate area of a circle from the radius..")
+print("Program to calculate area of a circle from the radius.")
 rad = float(input("Input the radius of the circle : "))
 print ("The area of the circle with radius "+str(rad)+" is: "+str(pi*rad**2) )
