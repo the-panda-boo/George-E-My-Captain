d96f53d4b13ce536acb48eea94a97778469874ee
George E George <georgee.ambat@gmail.com>
2021-04-18 20:01:05 +0530

    Print Positive

--- print_positive_nos.py
+++ print_positive_nos.py
@@ -1,11 +1,18 @@
-test_string = "There are 25 apples for 44 persons 56, 6"
-  
-# printing original string 
-print("The original string : " + test_string)
-  
-# using List comprehension + isdigit() +split()
-# getting numbers from string 
-res = [int(i) for i in test_string.split() if i.isdigit()]
-  
-# print result
-print("The numbers list is : " + str(res))
+# Enter number of elements with Instruction
+n = int(input("Enter number of elements : "))
+ 
+# Instructions
+print("Enter the elements below, use space and press enter only after inputing all the elements.")
+# Below line read inputs from user using map() function 
+a = list(map(int,input("\nEnter the numbers : ").strip().split()))[:n]
+
+#for loop calculating each element and creating element
+positive = []
+for num in list(a):
+    if (num>0): 
+        positive.append(num)
+print(len(positive))
+if (len(positive) == 0):
+    print("All the elements you've entered are negative.")
+else:
+    print("The positive elements are:", positive)
